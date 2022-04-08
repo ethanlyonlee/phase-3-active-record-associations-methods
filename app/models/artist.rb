91defd0eab4bf34dd28df1317bdace42f6b0765f
Ethan Lee <wxliyc@gmail.com>
2022-04-08 17:07:04 -0500

    commit tree 7f59f6b700c425e98cb7dfe7a2c8b642f5c8f5ee

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,10 +3,12 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
+    self.genre.songs.first
     #return the genre of the artist's first saved song
   end
 
   def song_count
+    self.count
     #return the number of songs associated with the artist
   end
 
